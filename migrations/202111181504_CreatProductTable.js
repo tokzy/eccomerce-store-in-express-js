bdaf0a2d657b348ab6fb8d17c684a54ac9bb469f
netsage <isaiahtokunbo11@gmail.com>
2021-11-18 16:59:08 +0100

    save

--- migrations/202111181504_CreatProductTable.js
+++ migrations/202111181504_CreatProductTable.js
@@ -8,6 +8,14 @@ module.exports = {
         type: Sequelize.DataTypes.INTEGER, 
         autoIncrement: true
       },
+      category_id: {
+      	allowNull: true,
+      	type: Sequelize.DataTypes.INTEGER
+      },
+      status: {
+      	allowNull: false,
+      	type: Sequelize.DataTypes.DECIMAL(10,2)
+      },
       name: {
       	allowNull: false,
       	type: Sequelize.DataTypes.STRING
@@ -15,6 +23,18 @@ module.exports = {
       price: {
       	allowNull: false,
       	type: Sequelize.DataTypes.DECIMAL(10,2)
+      },
+      cover_image: {
+      	allowNull: false,
+      	type: Sequelize.DataTypes.STRING
+      },
+      weight: {
+      	allowNull:true,
+      	type: Sequelize.DataTypes.DECIMAL(10,2)
+      },
+      descriptions: {
+      	allowNull: true,
+      	type: Sequelize.DataTypes.TEXT
       },
       createdAt: {
         allowNull: false,
