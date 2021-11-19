6403a2a2bf115a5e29727f12c05e15fbd10309e1
netsage <isaiahtokunbo11@gmail.com>
2021-11-19 16:59:42 +0100

    save

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,12 +1,18 @@
+const {Category} = require("../models");
+
+async function saveCategory() {
+let response = await Category.create({ name: "Jane"});
+return await response;
+}
 
 exports.getHome =  function(req, res, next) {
 res.render('index', { title: 'Express' });
 }
 
 exports.getCategory =  function(req, res, next) {
-    res.render('category');
+res.render('category');
 }
 
 exports.getContact =  function(req, res, next) {
-    res.render('contact');
+ res.render('contact');   
 }
