ba3a52cb4279a33dbfc9089a3266d59417b1d8fd
netsage <isaiahtokunbo11@gmail.com>
2021-11-20 15:01:32 +0100

    save

--- controllers/category.js
+++ controllers/category.js
@@ -1,25 +1,24 @@
 const {Category,Products,Brands} = require("../models");
 
-
 async function fetchallProducts(id){
 let category = Category.findAll();
 let brands = Brands.findAll();  
-let brandfetch = Products.findAll({
+let catfetch = Products.findAll({
 where: {
-brand_id: id
+category_id: id
 }
 });  
 try{
-let response = await Promise.all([category,brands,brandfetch]);
+let response = await Promise.all([category,brands,catfetch]);
 return await response;  
 }catch(e){
 return console.log(e);
 }   
 }
 
-exports.getProductsByBrand =  (req, res, next) => {
-id = req.params.brandId;
+exports.getProductsByCategory =  (req, res, next) => {
+id = req.params.catId;
 return fetchallProducts(id).then(values =>{
-res.render("category", {title:'brands',categories:values[0],brands:values[1],products:values[2]});
+res.render("category", {title:'category',categories:values[0],brands:values[1],products:values[2]});
 }).catch(e => console.log(e));
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const {getHome,getCategory,getContact} = require('../controllers/pages');
 const {getProductsByBrand} = require('../controllers/brand');
+const {getProductsByCategory} = require('../controllers/category');
 
 var router = express.Router();
 
@@ -8,7 +9,7 @@ router.get('/', getHome);
 router.get('/category', getCategory);
 router.get('/contact', getContact);
 router.get('/brand/:brandId',getProductsByBrand);
-
+router.get('/category/:catId',getProductsByCategory);
 
 
 module.exports = router;
