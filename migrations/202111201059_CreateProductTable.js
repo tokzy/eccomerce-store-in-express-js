8599fca38209bfa69e571ac3faa68b35bc0a25c8
netsage <isaiahtokunbo11@gmail.com>
2021-11-20 11:02:18 +0100

    save

---
+++ migrations/202111201059_CreateProductTable.js
@@ -0,0 +1,18 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+      return queryInterface.sequelize.transaction(t => {
+        return Promise.all([
+          queryInterface.addColumn('brand_id', {
+            type: Sequelize.DataTypes.INTEGER
+          }, { transaction: t })
+        ]);
+      });
+    },
+    down: (queryInterface, Sequelize) => {
+      return queryInterface.sequelize.transaction(t => {
+        return Promise.all([
+          queryInterface.removeColumn('brand_id', { transaction: t })
+        ]);
+      });
+    }
+  };
