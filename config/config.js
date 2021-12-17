d658608c83bbf0f40c5b319c0b5394459c37f5be
netsage <isaiahtokunbo11@gmail.com>
2021-12-17 09:28:42 +0100

    updated sequelize prod config

--- config/config.js
+++ config/config.js
@@ -18,10 +18,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.userName,
+    "password": process.env.Password,
+    "database": process.env.database,
+    "host": process.env.host,
+    "dialect": process.env.dialect,
+    "port": process.env.pg_port
   }
 }
