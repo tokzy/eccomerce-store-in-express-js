7ac54db5803b13832ed04f4e0d231525b5ad469c
netsage <isaiahtokunbo11@gmail.com>
2021-12-17 16:53:22 +0100

    edit a migration file

--- migrations/202111211218_UpdateReviewProduct.js
+++ migrations/202111211218_UpdateReviewProduct.js
@@ -2,12 +2,12 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Productreview', 
-    'name', { type: Sequelize.DataTypes.STRING}
+    'name2', { type: Sequelize.DataTypes.STRING}
     );
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.removeColumn('Productreview', 
-    'name', { type: Sequelize.DataTypes.STRING}
+    'name2', { type: Sequelize.DataTypes.STRING}
     );
   }
 };
